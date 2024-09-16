bb80f75db1184ca716b6f5ae9b483265d8747413
Jorge <jomruizgo@gmail.com>
2024-09-16 14:49:48 -0500

    HU8: Spring security and Jwt configured.

--- src/main/java/com/emazon/msvc_transaction/adapters/driving/controller/SaleController.java
+++ src/main/java/com/emazon/msvc_transaction/adapters/driving/controller/SaleController.java
@@ -1,10 +1,19 @@
 package com.emazon.msvc_transaction.adapters.driving.controller;
 
 import com.emazon.msvc_transaction.domain.util.Constants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = Constants.API_SALE_PATH)
 public class SaleController {
+
+    @PostMapping("")
+    public ResponseEntity<String> sell(){
+        String regard = "Hello client user from transaction microservice.";
+        return new ResponseEntity<>(regard, HttpStatus.OK);
+    }
 }
--- src/main/java/com/emazon/msvc_transaction/adapters/driving/controller/SupplyController.java
+++ src/main/java/com/emazon/msvc_transaction/adapters/driving/controller/SupplyController.java
@@ -1,10 +1,19 @@
 package com.emazon.msvc_transaction.adapters.driving.controller;
 
 import com.emazon.msvc_transaction.domain.util.Constants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = Constants.API_SUPPLY_PATH)
 public class SupplyController {
+
+    @PostMapping("")
+    public ResponseEntity<String> add(){
+        String regard = "Hello warehouse assistant user from transaction microservice.";
+        return new ResponseEntity<>(regard, HttpStatus.OK);
+    }
 }
